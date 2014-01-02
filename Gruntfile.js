d4683cc0fd29d13be4a356544cd604a9d73e514e
Instituto Dom Jose <edmo.jeova@idj.com.br>
2014-01-02 13:47:46 -0300

    Ignore

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,7 +15,7 @@ module.exports = function( grunt ) {
     
     // js minifcation options
   
-    uglify: {
+    uglify: { 
         'lib/js/jquery.min.js': 'bower_components/jquery/jquery.js',
         'lib/js/angular.min.js': 'bower_components/angular/angular.js',
      },
@@ -23,22 +23,12 @@ module.exports = function( grunt ) {
     // concat options
     concat : {
         task: {
-            src: [ 'bower_components/bootstrap/dist/css/bootstrap.css', 'build/css/style.css' ],
-            dest: 'assets/css/all.css',
+            src: [ 'bower_components/bootstrap/dist/css/bootstrap.css', 'lib/css/style.css' ],
+            dest: 'assets/css/all.css'
 
-            src: ['lib/js/jquery.min.js', 'lib/js/jquery.min.js'],
-            dest: 'assets/js/all.js'
         }
       },
 
-    // concat options
-    concat : {
-        task: {
-
-            src: ['lib/js/jquery.min.js', 'lib/js/jquery.min.js'],
-            dest: 'assets/js/all.js'
-        }
-      },
     });
 
     // Registrando task
